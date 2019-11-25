381de665bac5a6ca4e0ac06dd3e509ff7150fb7e
rohan <rst170000@utdallas.edu>
2019-11-25 14:31:03 -0600

    test line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test version controll intellij");
+        System.out.println("test version control intellij");
     }
 }
