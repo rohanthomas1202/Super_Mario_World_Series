523d1621a7dd9c6b91b9e03a8ea779fdd6d51195
rohan <rst170000@utdallas.edu>
2019-11-26 01:35:43 -0600

    function round added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Galaxy");
+    public static void main(String[] args) throws IOException {
+        File file = new File("keyfile.txt");
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null)
+            System.out.println(st);
     }
 }
+
